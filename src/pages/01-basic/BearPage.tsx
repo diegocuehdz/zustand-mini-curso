6d8ea9262b9043e2e3805ce5ad65306d7fbb5d5a
Dieguinho Pinho <diegocuehdz@gmail.com>
2025-07-15 01:40:20 -0600

    feat: add computed properties and methods

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,3 +1,4 @@
+import { useShallow } from 'zustand/shallow';
 import { WhiteCard } from '../../components';
 import { useBearStore } from '../../stores';
 
@@ -11,6 +12,7 @@ export const BearPage = () => {
         <BlackBears/>
         <PolarBears/>
         <PandaBears/>
+        <BearDisplay/>
       </div>
     </>
   );
@@ -37,6 +39,9 @@ export const PolarBears = () => {
   const polarBears = useBearStore((state) => state.polarBears)
   const increasePolarBears = useBearStore((state) => state.increasePolarBears)
 
+  // This causes the component to re-render because your subscribing to the whole state changes not just what you're interested
+  // const { polarBears, increasePolarBears} = useBearStore((state) => state)
+
   return (
     <WhiteCard centered>
       <h2>Osos Polares</h2>
@@ -61,6 +66,29 @@ export const PandaBears = () => {
         <span className="text-3xl mx-2 lg:mx-10"> { pandaBears} </span>
         <button onClick={() => increasePandaBears(+1)}>-1</button>
       </div>
+    </WhiteCard>
+  )
+}
+
+
+
+export const BearDisplay = () => {
+  // Using shallow comparison to avoid unnecessary re-renders, compares object and if they are the same it won't re-render
+  // const bears = useBearStore(useShallow((state) => state.bears));
+
+  const bears = useBearStore((state) => state.bears);
+
+  const doNothing = useBearStore((state) => state.doNothing);
+  const addBear = useBearStore((state) => state.addBear);
+  const clearBears = useBearStore((state) => state.clearBears);
+
+  return (
+    <WhiteCard >
+      <h2>Osos</h2>
+      <button onClick={() => doNothing()}>Do Nothing</button>
+      <button className='mt-2' onClick={() => addBear()}>Add Bear</button>
+      <button className='mt-2' onClick={() => clearBears()}>Clear Bears</button>
+      <pre>{JSON.stringify(bears, null, 2)}</pre>
     </WhiteCard>
   )
 }
--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -1,21 +1,52 @@
 import { create } from 'zustand'
 
+interface Bear {
+  id: number;
+  name: string;
+}
+
 interface BearState {
   blackBears: number;
   pandaBears: number;
   polarBears: number;
 
+  bears: Bear[];
+  computed: {
+    totalBears: number;
+  };
+
   increaseBlackBears: (by: number) => void;
   increasePandaBears: (by: number) => void;
   increasePolarBears: (by: number) => void;
+
+  doNothing: () => void;
+  addBear: () => void;
+  clearBears: () => void;
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()((set, get) => ({
   blackBears: 10,
   pandaBears: 10,
   polarBears: 10,
 
+  bears: [{
+    id: 1,
+    name: 'Bear #1'
+  }],
+
+  computed: {
+    get totalBears(): number {
+      return get().blackBears + get().pandaBears + get().polarBears + get().bears.length;
+    }
+  },
+
   increaseBlackBears: (by) => set((state) => ({ blackBears: state.blackBears + by })),
   increasePandaBears: (by) => set((state) => ({ pandaBears: state.pandaBears + by })),
   increasePolarBears: (by) => set((state) => ({ polarBears: state.polarBears + by })),
+
+  doNothing: () => set((state) => ({ bears: [...state.bears] })),
+  addBear: () => set((state) => ({
+    bears: [...state.bears, { id: state.bears.length + 1, name: `Bear #${state.bears.length + 1}` }]
+  })),
+  clearBears: () => set({ bears: [] })
 }))
